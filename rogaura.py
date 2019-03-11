2562d98ad558f2116bba0f7acdf480464a42bb4f
Will Roberts <will.roberts@rz.hu-berlin.de>
2019-03-11 20:28:33 +0100

    rogaura.py implement

--- rogaura.py
+++ rogaura.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+Control the RGB keyboard on the Asus ROG Strix GL503VD.
+
+rogaura.py
+(c) Will Roberts  11 March, 2019
+"""
+
+from collections import namedtuple
+from enum import Enum
 import array
 import binascii
+import inspect
 import sys
 import usb.core
 import usb.util
@@ -10,23 +20,40 @@ import usb.util
 ASUS_VENDOR_ID = 0x0b05
 ASUS_PRODUCT_ID = 0x1869
 
+class Color(namedtuple('Color', ['red', 'green', 'blue'])):
+    __slots__ = ()
+    @classmethod
+    def fromhexstr(cls, sval):
+        return cls(*list(binascii.unhexlify(sval.lstrip('#'))))
+
+class Speed(Enum):
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+
+    @property
+    def bytevalue(self):
+        return [0xe1, 0xeb, 0xf5][self.value - 1]
+
+def toarray(sval):
+    arr = array.array('B')
+    arr.frombytes(binascii.unhexlify(sval))
+    return arr
+
+SET_MESSAGE = toarray('5db5000000000000000000000000000000')
+
 def acquire():
     dev = usb.core.find(idVendor=ASUS_VENDOR_ID, idProduct=ASUS_PRODUCT_ID)
     if dev is None:
         print('Could not find ASUS RGB keyboard.')
         sys.exit(1)
 
-    #print(dev)
-
     try:
-        # get the keyboard USB interface
         assert dev.bNumConfigurations == 1
         cfg = dev.configurations()[0]
         assert cfg.bNumInterfaces == 1
         iface = 0
 
-        #cfg = dev.get_active_configuration()
-        #iface = cfg[(0,0)]
         active_iface = False
         if dev.is_kernel_driver_active(iface):
             dev.detach_kernel_driver(iface)
@@ -34,10 +61,6 @@ def acquire():
     except usb.USBError as e:
         usb.util.dispose_resources(dev)
         raise e
-
-    # set the active configuration. With no arguments, the first
-    # configuration will be the active one
-    #dev.set_configuration()
 
     try:
         usb.util.claim_interface(dev, iface)
@@ -56,147 +79,144 @@ def release(dev, iface, active_iface):
     usb.util.dispose_resources(dev)
 
 def single_static(color):
-    pass
+    arr = toarray('5db30000ff000000000000000000000000')
+    arr[4] = color.red
+    arr[5] = color.green
+    arr[6] = color.blue
+    return [arr, SET_MESSAGE]
 
 def single_breathing(color1, color2, speed):
-    pass
+    # ('breathe slow', '5db3000108fff0e10001ff000000000000'),
+    # ('breathe med ', '5db3000108fff0eb0001ff000000000000'),
+    # ('breathe fast', '5db3000108fff0f50001ff000000000000')]
+    arr = toarray('5db3000108fff0eb0001ff000000000000')
+    arr[4] = color1.red
+    arr[5] = color1.green
+    arr[6] = color1.blue
+    arr[10] = color2.red
+    arr[11] = color2.green
+    arr[12] = color2.blue
+    arr[7] = speed.bytevalue
+    return [arr, SET_MESSAGE]
 
 def single_colorcycle(speed):
-    pass
+    # ('rainbow cycle slow', '5db30002ff0000e1000000000000000000'),
+    # ('rainbow cycle med ', '5db30002ff0000eb000000000000000000'),
+    # ('rainbow cycle fast', '5db30002ff0000f5000000000000000000')
+    arr = toarray('5db30002ff0000eb000000000000000000')
+    arr[7] = speed.bytevalue
+    return [arr, SET_MESSAGE]
 
 def multi_static(color1, color2, color3, color4):
-    pass
+    # '5db30100ff0000eb000000000000000000',
+    # '5db30200ffff00eb000000000000000000',
+    # '5db3030000ffffeb000000000000000000',
+    # '5db30400ff00ffeb000000000000000000',
+    result = []
+    for idx, color in enumerate([color1, color2, color3, color4], 1):
+        arr = toarray('5db30100ff0000eb000000000000000000')
+        arr[2] = idx
+        arr[4] = color.red
+        arr[5] = color.green
+        arr[6] = color.blue
+        result.append(arr)
+    return result + [SET_MESSAGE]
 
 def multi_breathing(color1, color2, color3, color4, speed):
-    pass
+    # '5db30101ff0000eb000000000000000000',
+    # '5db30201ffff00eb000000000000000000',
+    # '5db3030100ffffeb000000000000000000',
+    # '5db30401ff00ffeb000000000000000000',
+    # '5db5000000000000000000000000000000'
+    result = []
+    for idx, color in enumerate([color1, color2, color3, color4], 1):
+        arr = toarray('5db30101ff0000eb000000000000000000')
+        arr[2] = idx
+        arr[4] = color.red
+        arr[5] = color.green
+        arr[6] = color.blue
+        arr[7] = speed.bytevalue
+        result.append(arr)
+    return result + [SET_MESSAGE]
 
-CONVOS = [
-    ('static rainbow',
-     ['5db30100ff0000eb000000000000000000',
-      '5db30200ffff00eb000000000000000000',
-      '5db3030000ffffeb000000000000000000',
-      '5db30400ff00ffeb000000000000000000',
-      '5db5000000000000000000000000000000']),
-    #('',
-    # ['5db4000000000000000000000000000000',
-    #  '5db5000000000000000000000000000000']),
-    ('static red',
-     [#'5d4153555320546563682e496e632e0000  ]ASUS Tech.Inc.',
-      #'5d05203100080000000000000000000000',
-      '5db30000ff000000000000000000000000',
-      '5db5000000000000000000000000000000']),
-    ('rainbow cycle med',
-     ['5db30002ff0000eb000000000000000000',
-      '5db5000000000000000000000000000000']),
-    ('rainbow cycle slow',
-     ['5db30002ff0000e1000000000000000000',
-      '5db5000000000000000000000000000000']),
-    ('rainbow cycle med',
-     ['5db30002ff0000eb000000000000000000',
-      '5db5000000000000000000000000000000']),
-    ('rainbow cycle fast',
-     ['5db30002ff0000f5000000000000000000',
-      '5db5000000000000000000000000000000']),
-    ('breathe med',
-     ['5db3000108fff0eb0001ff000000000000',
-      '5db5000000000000000000000000000000']),
-    ('breathe slow',
-     ['5db3000108fff0e10001ff000000000000',
-      '5db5000000000000000000000000000000']),
-    ('breathe med',
-     ['5db3000108fff0eb0001ff000000000000',
-      '5db5000000000000000000000000000000']),
-    ('breathe fast',
-     ['5db3000108fff0f50001ff000000000000',
-      '5db5000000000000000000000000000000']),
-    ('static red',
-     ['5db30000ff000000000000000000000000',
-      '5db5000000000000000000000000000000']),
-    ('static rainbow',
-     ['5db30100ff0000eb000000000000000000',
-      '5db30200ffff00eb000000000000000000',
-      '5db3030000ffffeb000000000000000000',
-      '5db30400ff00ffeb000000000000000000',
-      '5db5000000000000000000000000000000']),
-    ('rainbow breathe med',
-     ['5db30101ff0000eb000000000000000000',
-      '5db30201ffff00eb000000000000000000',
-      '5db3030100ffffeb000000000000000000',
-      '5db30401ff00ffeb000000000000000000',
-      '5db5000000000000000000000000000000']),
-    ('rainbow breathe slow',
-     ['5db30101ff0000e1000000000000000000',
-      '5db30201ffff00e1000000000000000000',
-      '5db3030100ffffe1000000000000000000',
-      '5db30401ff00ffe1000000000000000000',
-      '5db5000000000000000000000000000000']),
-    ('rainbow breathe med',
-     ['5db30101ff0000eb000000000000000000',
-      '5db30201ffff00eb000000000000000000',
-      '5db3030100ffffeb000000000000000000',
-      '5db30401ff00ffeb000000000000000000',
-      '5db5000000000000000000000000000000']),
-    ('rainbow breathe fast',
-     ['5db30101ff0000f5000000000000000000',
-      '5db30201ffff00f5000000000000000000',
-      '5db3030100fffff5000000000000000000',
-      '5db30401ff00fff5000000000000000000',
-      '5db5000000000000000000000000000000']),
-    ('rainbow breathe slow',
-     ['5db30101ff0000e1000000000000000000',
-      '5db30201ffff00e1000000000000000000',
-      '5db3030100ffffe1000000000000000000',
-      '5db30401ff00ffe1000000000000000000',
-      '5db5000000000000000000000000000000']),
-    ('static rainbow',
-     ['5db30100ff0000eb000000000000000000',
-      '5db30200ffff00eb000000000000000000',
-      '5db3030000ffffeb000000000000000000',
-      '5db30400ff00ffeb000000000000000000',
-      '5db5000000000000000000000000000000']),
-    #('',
-    # ['5db4000000000000000000000000000000',
-    #  '5db5000000000000000000000000000000']),
-    ('static rainbow',
-     ['5db30100ff0000eb000000000000000000',
-      '5db30200ffff00eb000000000000000000',
-      '5db3030000ffffeb000000000000000000',
-      '5db30400ff00ffeb000000000000000000',
-      '5db5000000000000000000000000000000']),
-    #('',
-    # ['5db4000000000000000000000000000000',
-    #  '5db5000000000000000000000000000000']),
+def rainbow():
+    return multi_static(Color.fromhexstr('ff0000'), Color.fromhexstr('ffff00'), Color.fromhexstr('00ffff'), Color.fromhexstr('ff00ff'))
+
+def red():
+    return single_static(Color.fromhexstr('ff0000'))
+
+def white():
+    return single_static(Color.fromhexstr('ffffff'))
+
+COLOR_FUNCS = [
+    single_static,
+    single_breathing,
+    single_colorcycle,
+    multi_static,
+    multi_breathing,
+    rainbow,
+    red,
+    white,
 ]
 
+USAGE = '''rogaura.py -- Asus Rog RGB keyboard control
+(c) 2019 Will Roberts
+
+Syntax:
+    python3 rogaura.py MODE [args...]
+
+MODE is one of {}.
+'''
+
+def parseargs(argv):
+    # first identify the function the user wants
+    mapping = dict((x.__name__, x) for x in COLOR_FUNCS)
+    name = None
+    if argv:
+        name = argv[0]
+    if not argv or '-?' in argv or '-h' in argv or '--help' in argv or name not in mapping:
+        print(USAGE.format(', '.join([x.__name__ for x in COLOR_FUNCS])))
+        if name and name not in mapping:
+            print('Unknown MODE {}'.format(name))
+        sys.exit(1)
+    func = mapping[name]
+    # then figure out the arguments to that function
+    argnames = inspect.signature(func).parameters.keys()
+    if len(argv[1:]) != len(argnames):
+        print('Arguments for MODE {}: {}'.format(name, ', '.join(argnames)))
+        print('\ncolor arguments should be given as hex values like ff0000 or #ff0000')
+        print('speed argument should be given as an integer: 1, 2, or 3')
+        sys.exit(1)
+    # we know that the arguments match up with the function signature,
+    # we just need to parse the values now
+    args = {}
+    for argname, value in zip(argnames, argv[1:]):
+        if argname.startswith('color'):
+            args[argname] = Color.fromhexstr(value)
+        elif argname == 'speed':
+            args[argname] = Speed(int(value))
+        else:
+            raise Exception('Undecipherable argument name {} in function {}'.format(argname, name))
+    # now we have the args, we use them to invoke the proper function
+    return func(**args)
+
 def main():
-    # replay recorded conversations
-    last_convo_name = None
-    untested_convo = None
-    for convo_name, convo in CONVOS:
-        if convo_name == '':
-            print('found untested convo.  last tested convo was {}'.format(last_convo_name))
-            untested_convo = convo
-            break
-        last_convo_name = convo_name
-    if untested_convo is None:
-        print('no untested convo found')
+    try:
+        messages = parseargs(sys.argv[1:])
+    except Exception as e:
+        print(e)
         sys.exit(1)
 
     dev, iface, active_iface = acquire()
 
-    for speech in untested_convo:
-        msg = array.array('B')
-        #msg.frombytes(binascii.unhexlify(b'5db30101ff0000eb000000000000000000'))
-        msg.frombytes(binascii.unhexlify(speech))
-        # msg
+    for message in messages:
         bmRequestType = 0x21
         bRequest = 0x09
         wValue = 0x035d
         wIndex = 0
 
-        print('Sending: {}'.format(speech))
         try:
-            dev.ctrl_transfer(bmRequestType, bRequest, wValue, wIndex, msg)
+            dev.ctrl_transfer(bmRequestType, bRequest, wValue, wIndex, message)
         except usb.USBError as e:
             release(dev, iface, active_iface)
             raise e
@@ -207,45 +227,3 @@ if __name__ == '__main__':
     main()
 
 sys.exit(0)
-
-
-
-# all control transfer packets defined in
-# https://github.com/MidhunSureshR/openauranb/blob/master/rog_aura.c
-# start with bmRequestType 0x21
-#
-# bin(0x21) => 00100001
-# 0: host-to-device
-# 01: class
-# 00001: interface
-#
-# then we have bRequest as 0x09
-# bin(0x09) =? 00001001 (9) SET_CONFIGURATION
-#
-# then we have wValue as 0x035D (861), wIndex is 0x0000, and wLength is 17
-#
-# packet payloads always start with 0x5d
-#
-# bin(0x5d) => 01011101
-#
-# 0: host-to-device
-# 10: vendor
-# 11101: reserved? (29)
-#
-# after that, we've got variously 0xb3 (color value), b4 (apply), or b5 (set)
-# bin(0xb4) = 10110100 = 180
-# bin(0xb5) = 10110101 = 181
-# bin(0xb6) = 10110110 = 182
-#
-# for 0xb4 and b5, after that we've got all zeroes
-#
-# we've always got 17 bytes in each message
-#
-# lsusb gives:
-# wMaxPacketSize     0x0040  1x 64 bytes
-# bInterval               4
-#
-# in the case of 0xb3, we follow with 0x0000, and then the color data
-# comes (3 bytes); after the 0x0000, the message length has room for
-# 13 bytes (4 x RGB + 1 byte mode?).  maybe the 0x0000 has mode
-# information?
